4488979d916c03a7bd9cd769df939c1d35de3907
Ian Gilman <ian@iangilman.com>
2017-08-18 10:01:49 -0700

    Peter's new version of kirkeboeger_show3

--- kirkeboeger_show3/index.php
+++ kirkeboeger_show3/index.php
@@ -4,7 +4,7 @@
 
 <head>
 
-<title>W3.CSS</title>
+<title>Vis arkivalie, den jeg vil bruge</title>
 <meta name="viewport" content="width=site-width, initial-scale=1.0">
 <meta charset="utf-8" />
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
@@ -15,16 +15,13 @@
 
 <body>
 
-
-
-
-
 <script>
 function updateForHash() {
   var hash = window.location.hash.replace(/^#/, '');
-  if (hash && /^page /.test(hash)) {
-    var hashNumber = hash.replace(/^side /, '');
+  if (hash && /^side/.test(hash)) {
+    var hashNumber = hash.replace(/^side/, '');
     var index = parseInt(hashNumber, 10) - 1;
+    console.log('Going to page index', index);
     viewer.goToPage(index);
   }
 }
@@ -32,7 +29,78 @@ function updateForHash() {
 
 
 
+
+
+
+<!-- Sidebar højre-->
+
+<div class="w3-sidebar w3-bar-block w3-card-1" style="width:25%;  right:0;background-color:#666666;  border-left: 1px solid #f6f6f6;  border-bottom: 0px solid #f6f6f6; overflow:hidden">
+
+
+
+
+  
+  <input type="text" style="-webkit-font-smoothing:antialiased;-webkit-transition:color .2s ease-in-out;-moz-transition:color .2s ease-in-out;-o-transition:color .2s ease-in-out;-ms-transition:color .2s ease-in-out;transition:color .2s ease-in-out;font-family: Lucida Blackletter; text-docoration:none; font-weight:bold; font-size: 34px; text-align:left; text-shadow: 5px 5px 10px #191919;color: #FFFFFF; background-color:#666666; margin-left:10px;border:none";id="my_text-input3" value="Genealogisk forum">
+  <br>
+ 
+
+  <div class="flex-container">
+<div class="flex-item"><input type="text" style="font-family: arial; font-size: 12px; font-weight:normal; text-align:center; color: #e1e1e1; background-color:#666666; width: 300px; margin-left:0px;border: 1px solid #f6f6f6; ";id="my_text-input3" value="Test bog "></div>
+ 
+ <hr>
+</div>
+
+  
+<img src="Praest_der_laeser.png" style="width:304px;height:228px;padding-left:10px;">
+  <hr>
+  
+
+  <!-- Links i højre sidebar -->
+
+  <a href="https://genealogiskforum.dk/"style="font-family: arial; font-weight: normal; font-size: 12px; color: lightgrey; margin-left:10px; class="w3-bar-item w3-button";>Velkommen <a/> <br>
+
+  <a href="https://genealogiskforum.dk/slaegtstrae/"style="font-family: arial; font-weight: normal; font-size: 12px; color: lightgrey; margin-left:10px; class="w3-bar-item w3-button">Slægtstræ </a> <br>
+
+  <a href="https://genealogiskforum.dk/dialogforum/"style="font-family: arial; font-weight: normal; font-size: 12px; color: lightgrey; margin-left:10px; class="w3-bar-item w3-button">Dialogforum </a> <br>
+
+  <a href="https://genealogiskforum.dk/Gf_Magasin/"style="font-family: arial; font-weight: normal; font-size: 12px; color: lightgrey; margin-left:10px; class="w3-bar-item w3-button">Gf Magasin </a> <br>
+
+  <a href="https://genealogiskforum.dk/gravsten_2/"style="font-family: arial; font-weight: normal; font-size: 12px; color: lightgrey; margin-left:10px; class="w3-bar-item w3-button">Red en gravsten </a> <br>
+
+  <a href="https://genealogiskforum.dk/tools/"style="font-family: arial; font-weight: normal; font-size: 12px; color: lightgrey; margin-left:10px; class="w3-bar-item w3-button">Værktøjer </a> <br>
+
+  <a href="https://genealogiskforum.dk/bestil_slaegtstrae/"style="font-family: arial; font-weight: normal; font-size: 12px; color: lightgrey; margin-left:10px; class="w3-bar-item w3-button">Bestil slægtstræ </a> <br>
+
+  <a href="https://genealogiskforum.dk/om_gf/"style="font-family: arial; font-weight: normal; font-size: 12px; color: lightgrey; margin-left:10px; class="w3-bar-item w3-button">Om Genealogisk forum </a> <br>
+
+  <a href="https://genealogiskforum.dk/medlemmer/"style="font-family: arial; font-weight: normal; font-size: 12px; color: lightgrey; margin-left:10px; class="w3-bar-item w3-button">Medlemmer </a> <br>
+
+  <a href="https://genealogiskforum.dk/tips_vejledninger/"style="font-family: arial; font-weight: normal; font-size: 12px; color: lightgrey; margin-left:10px; class="w3-bar-item w3-button">Tips og vejledninger </a> <br>
+
+  <a href="https://genealogiskforum.dk/kontakt/"style="font-family: arial; font-weight: normal; font-size: 12px; color: lightgrey; margin-left:10px; class="w3-bar-item w3-button">Kontakt </a> <br>
+
+<br>  
+
+
+    
+
+</script>
+<body onload="myNavi()">
+  
+</div>
+
+
+
+<div style="margin-right:20%">
+
+
+
+
+
 <!-- Page Content -->
+
+
+
 
 
 <!-- OSD start -->
@@ -73,15 +141,16 @@ function updateForHash() {
 </span>    
 
 
-<div class="flex-container">
+
+
 
 <!-- My own buttons before -->
 
 
+<div class="flex-container">
 
-
-<!-- First page -->	
-<div class="flex-item"><input type="button" button title="Første side" style="width: 30px; text-align:center; color: #f6f6f6; background-color:#666666; border:none; margin-left:20px";id="onefirst"; onclick="firstOne()" value="|<"></div> 
+<!-- First page --> 
+<div class="flex-item"><input type="button" button title="Første side" style="width: 40px; margin-right:3px; text-align:center; color: #f6f6f6; background-color:#666666; border:none; margin-left:20px";id="onefirst"; onclick="firstOne()" value="|<"></div> 
 
 
  <script>
@@ -93,7 +162,7 @@ function firstOne() {
 
 
 <!-- -1 page -->
-<div class="flex-item"><input type="button" button title="1 side tilbage"style="width: 30px; text-align:center; color: #f6f6f6; background-color:#666666; border:none;  margin-left:20px";id="oneminus"; onclick="minusOne()" value="<1"></div> 
+<div class="flex-item"><input type="button" button title="1 side tilbage"style="width: 40px; margin-right:3px; text-align:center; color: #f6f6f6; background-color:#666666; border:none;  margin-left:20px";id="oneminus"; onclick="minusOne()" value="<1"></div> 
 
  <script>
 function minusOne() {
@@ -102,7 +171,7 @@ function minusOne() {
 </script>
 
 <!-- -5 pages -->
-<div class="flex-item"><input type="button" button title="5 sider tilbage"style="width: 30px; text-align:center; color: #f6f6f6; background-color:#666666; border:none;  margin-left:20px";id="fiveminus"; onclick="minusFive()" value="<5"></div> 
+<div class="flex-item"><input type="button" button title="5 sider tilbage"style="width: 40px; margin-right:3px; text-align:center; color: #f6f6f6; background-color:#666666; border:none;  margin-left:20px";id="fiveminus"; onclick="minusFive()" value="<5"></div> 
 
  <script>
 function minusFive() {
@@ -111,7 +180,7 @@ function minusFive() {
 </script>
 
 <!-- -10 pages -->
-<div class="flex-item"><input type="button" button title="10 sieder tilbage"style="width: 40px; text-align:center; color: #f6f6f6; background-color:#666666; border:none; margin-right:20px;  margin-left:20px";id="tenminus"; onclick="minusTen()" value="<10"></div> 
+<div class="flex-item"><input type="button" button title="10 sieder tilbage"style="width: 40px; margin-right:3px; text-align:center; color: #f6f6f6; background-color:#666666; border:none; margin-right:20px;  margin-left:20px";id="tenminus"; onclick="minusTen()" value="<10"></div> 
 
  <script>
 function minusTen() {
@@ -120,17 +189,17 @@ function minusTen() {
 </script>
 
  <!-- Input field for page search -->
-  <div class="flex-item"><input type="text" style="width: 45px; text-align:right; color: #f6f6f6; background-color:#666666; border:none; margin-right:0px; margin-left:20px";id="my_text-input" value="Side"></div>
+  <div class="flex-item"><input type="text" style="width: 45px; text-align:right; color: #f6f6f6; background-color:#666666; border:none; margin-right:0px; margin-left:5px";id="my_text-input" value="Side"></div>
 
   <div class="flex-item"><input type="text" style="width: 45px; text-align:center; color: #666666; background-color:#f6f6f6; border: 1px solid black; margin-left:20px"; id="my-page-input" value="1"></div>
 
-  <div class="flex-item"><input type="text" style="width: 55px; text-align:left; color: #f6f6f6; background-color:#666666; border:none; margin-left:20px; margin-right:20px";id="my_text-input_2" value="af 214"></div> 
+  <div class="flex-item"><input type="text" style="width: 45px; text-align:left; color: #f6f6f6; background-color:#666666; border:none; margin-left:20px; margin-right:5px";id="my_text-input_2" value="af 18"></div> 
 
 
 
    <!-- My own buttons after -->
     <!-- +1 page -->
-<div class="flex-item"><input type="button" button title="1 side frem"style="width: 30px; text-align:center; color: #f6f6f6; background-color:#666666; margin-left:20px; border:none";id="oneplus"; onclick="plusOne()" value="1>"></div> 
+<div class="flex-item"><input type="button" button title="1 side frem"style="width: 30px; margin-right:3px; text-align:center; color: #f6f6f6; background-color:#666666; margin-left:20px; border:none";id="oneplus"; onclick="plusOne()" value="1>"></div> 
 
  <script>
 function plusOne() {
@@ -140,7 +209,7 @@ function plusOne() {
 
     <!-- +5 pages -->
 
-  <div class="flex-item"><input type="button" button title="5 sider frem"style="width: 30px; text-align:center; color: #f6f6f6; background-color:#666666; border:none; margin-left:20px";id="fiveplus"; onclick="plusFive()" value="5>"></div> 
+  <div class="flex-item"><input type="button" button title="5 sider frem"style="width: 30px; margin-right:3px; text-align:center; color: #f6f6f6; background-color:#666666; border:none; margin-left:20px";id="fiveplus"; onclick="plusFive()" value="5>"></div> 
 
  <script>
 function plusFive() {
@@ -149,7 +218,7 @@ function plusFive() {
 </script>
 
     <!-- +10 pages -->
-<div class="flex-item"><input type="button" button title="10 sieder frem"style="width: 40px; text-align:center; color: #f6f6f6; background-color:#666666; border:none; margin-left:20px";id="tenplus"; onclick="plusTen()" value="10>"></div> 
+<div class="flex-item"><input type="button" button title="10 sieder frem"style="width: 40px; margin-right:3px; text-align:center; color: #f6f6f6; background-color:#666666; border:none; margin-left:20px";id="tenplus"; onclick="plusTen()" value="10>"></div> 
 
  <script>
 function plusTen() {
@@ -158,7 +227,7 @@ function plusTen() {
 </script>
 
     <!-- Last page -->
-<div class="flex-item"><input type="button" button title="Sidste side"style="width: 30px; text-align:center; color: #f6f6f6; background-color:#666666; border:none; margin-left:20px";id="onelast"; onclick="lastOne()" value=">|"></div> 
+<div class="flex-item"><input type="button" button title="Sidste side"style="width: 30px; margin-right:3px; text-align:center; color: #f6f6f6; background-color:#666666; border:none; margin-left:20px";id="onelast"; onclick="lastOne()" value=">|"></div> 
 
  <script>
 function lastOne() {
@@ -168,53 +237,86 @@ function lastOne() {
 
 
  
-<div id="openseadragon1" style="width: site-width; height: 600px; border-top: 1px solid #f6f6f6;background-color:#666666";"margin: auto;></div>
+</div>
+
+
+
+<div id="openseadragon1" style="width: site-width; height:650px; border-top: 1px solid #f6f6f6;background-color:#666666";"margin: auto;></div>
 
 
 
 
 
 <!-- OSD -->
-<script src="/openseadragon/openseadragon.min.js"></script>
-<script src="/openseadragon/zoomifytilesource.js"></script>
++<script src="https://cdn.jsdelivr.net/npm/openseadragon@2.3/build/openseadragon/openseadragon.min.js"></script>
 
 
 
 <script type="text/javascript">
     var viewer = OpenSeadragon({
-    tileSources: {
-    type: 'image',
-    url: OpenSeadragon.getUrlParameter('img')
-  },
+
+
+
+
         id: "openseadragon1",
-        prefixUrl: "/openseadragon/images/",
-  
+        
+        prefixUrl: "https://cdn.jsdelivr.net/npm/openseadragon@2.3/build/openseadragon/images/",
+  //showNavigator:  true,
+  navigatorId:   "navigatorDiv",
+
+        //navigatorPosition: "ABSOLUTE",
+        //navigatorTop:      "900px",
+        //navigatorLeft:     "750px",
+        //navigatorHeight:   "120px",
+        //navigatorWidth:    "100px",
+    //navigatorPosition:   "BOTTOM_RIGHT",
+
+      //navigatorHeight:   "100px",
+        //navigatorWidth:    "60px",
+
      sequenceMode: true,
-     preserveViewport: true,
-     defaultZoomLevel: 0.3,
-     visibilityRatio: 0.5,
-     constrainDuringPan: true,
-     homeButton:     "home",
-     fullPageButton: "full-page",
-     showSequenceControl: false,
+  preserveViewport: true,
+  //showReferenceStrip: true,
+    referenceStripScroll: 'horizontal',
+  defaultZoomLevel: 0.3,
+    visibilityRatio: 0.5,
+    constrainDuringPan: true,
+    // Show rotation buttons
+    //showRotationControl: true,
+    //zoomInButton:   "zoom-in",
+    //zoomOutButton:  "zoom-out",
+    homeButton:     "home",
+    fullPageButton: "full-page",
+    //nextButton:     "next",
+    //previousButton: "previous",
+    //showZoomControl: false,
+    //showHomeControl: false,
+    showSequenceControl: false,
+    
+
+
+
+
+    
+    
+    
   });
 
 
 
-<script type="text/javascript">
-   var input = document.getElementById('my-page-input');
 
-    input.addEventListener('keypress', function(event) {
+<!-- Sikrer at vi kan bruge Enter tast tl at søge efter en bestemt side -->
+   var input = document.getElementById('my-page-input');
+  input.addEventListener('keypress', function(event) {
         if (event.which === 13) { // Return key
             viewer.goToPage(parseInt(input.value, 10)-1);
         }
     });
 
+    
+<!-- Kigger efter at opdatere sidetallet i sidenummerboksen -->
+<!-- og kigger efter at opdatere url til en individuel url -->
     viewer.addHandler("page", function (data) {
-        input.value = ( data.page + 1 );
-    });
-
-        viewer.addHandler("page", function (data) {
         input.value = ( data.page + 1 );
       location.hash = "side" + (data.page + 1);
   if (!location.hash.replace(/^#/, '')) {
@@ -222,22 +324,24 @@ function lastOne() {
 }    
     });
 
-     viewer.addHandler('open', function() {
+<!-- Aktiverer separat url (hash) -->
+    viewer.addOnceHandler('open', function() {
   updateForHash();
 });
-   
 
-  viewer.open({
-  type: 'image',
-  url: 'http://ao.sa.dk/ao/data.ashx?bid=24530382'
-});
-      
+
+
+
+
+
+
+
+
+
 </script>
 
-
-
+</div>
 
 </body>
-
 </html>
 
